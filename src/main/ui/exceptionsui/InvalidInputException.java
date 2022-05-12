3c15d2d5f8352327025c4c25d11af6ed3d1ac31e
Volcapan <105190275+Volcapan@users.noreply.github.com>
2022-05-11 19:01:48 -0700

    Add files via upload

---
+++ src/main/ui/exceptionsui/InvalidInputException.java
@@ -0,0 +1,5 @@
+package ui.exceptionsui;
+
+// Represents an exception when invalid input is given
+public class InvalidInputException extends Exception{
+}
---
+++ src/main/ui/exceptionsui/PauseGameException.java
@@ -0,0 +1,5 @@
+package ui.exceptionsui;
+
+//Represents an exception when Tank Duel is paused
+public class PauseGameException extends Exception {
+}
